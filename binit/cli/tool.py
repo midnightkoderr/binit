c5fe6c20589e8272276cf90a1f734f1935c94dae
midnightkoderr <175213477+midnightkoderr@users.noreply.github.com>
2026-03-24 17:32:17 +0530

    refactor: move install command as a sub under tool command

--- binit/cli/tool.py
+++ binit/cli/tool.py
@@ -3,11 +3,16 @@ import click
 from binit.installer import Installer
 
 
-@click.command(name='install')
+@click.group(name='tool')
+def tool():
+    '''Manage installed tools'''
+
+
+@tool.command(name='install')
 @click.option('--github-repo', '-r', required=True, help='GitHub repo as owner/repo or full URL')
 def install(github_repo: str):
     '''Install a binary from the latest GitHub release'''
     try:
-        tool = Installer(github_repo).run()
+        Installer(github_repo).run()
     except ValueError as e:
         raise click.BadParameter(str(e), param_hint='--github-repo')
