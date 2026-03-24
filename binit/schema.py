fe5f68597e59d5904fae82de1c78927271c0f818
midnightkoderr <175213477+midnightkoderr@users.noreply.github.com>
2026-03-24 15:12:53 +0530

    feat(schema): add config schema

---
+++ binit/schema.py
@@ -0,0 +1,45 @@
+from marshmallow import INCLUDE, Schema, fields, post_load, validate
+
+from binit.core.constants import ARCH_ALIASES, SUPPORTED_PLATFORMS
+from binit.models import ConfigModel, ToolModel
+
+
+class ToolSchema(Schema):
+    name = fields.Str(required=True)
+    repo = fields.Url(required=True)
+    asset = fields.Str(required=True)
+    release = fields.Str(required=True)
+    version = fields.Str(required=True, validate=validate.Regexp(r'^\d+\.\d+\.\d+$', error='Version must be in X.Y.Z format'))
+    homepage = fields.Url(allow_none=True)
+    updated_at = fields.DateTime(required=True)
+    installed_at = fields.DateTime(allow_none=True)
+    description = fields.Str(allow_none=True)
+    license = fields.Str(allow_none=True)
+    binary = fields.Str(required=True)
+
+
+    class Meta:
+        unknown = INCLUDE
+
+
+    @post_load
+    def make(self, data, **kwargs):
+        return ToolModel(**data)
+
+
+class ConfigSchema(Schema):
+    binit_version = fields.Str(required=True)
+    os = fields.Str(required=True, validate=validate.OneOf(SUPPORTED_PLATFORMS))
+    arch = fields.Str(required=True, validate=validate.OneOf(ARCH_ALIASES.keys()))
+    init_at = fields.DateTime(required=True)
+    base_dir = fields.Str(required=True)
+    installed_tools = fields.Dict(keys=fields.Str(), values=fields.Nested(ToolSchema), required=True)
+
+
+    class Meta:
+        unknown = INCLUDE
+
+
+    @post_load
+    def make(self, data, **kwargs):
+        return ConfigModel(**data)
