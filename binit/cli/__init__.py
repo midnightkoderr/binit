8a95d9d5842ffdb8693e1a69047a90df57a139ae
midnightkoderr <175213477+midnightkoderr@users.noreply.github.com>
2026-04-29 12:59:34 +0530

    refactor: replace hardcoded VERSION constant with importlib.metadata

--- binit/cli/__init__.py
+++ binit/cli/__init__.py
@@ -1,11 +1,11 @@
 import click
+from importlib.metadata import version
 
 from binit.cli import config, init, tool
-from binit.core.constants import VERSION
 
 
 @click.group(invoke_without_command=True)
-@click.version_option(version=VERSION, prog_name='binit')
+@click.version_option(version=version('binit'), prog_name='binit')
 @click.pass_context
 def cli(ctx: click.Context):
     '''binit - minimal binary installer'''
